beac5a6f02f3c1d97563f9c121989a1690420135
sudhanshu <sudhanshumll23@gmail.com>
2021-06-19 14:30:08 +0530

    kk

--- Async/asyncSerial.js
+++ Async/asyncSerial.js
@@ -1,2 +1,7 @@
 let fs = require("fs");
-let files = ["../f1.txt" , "../f2.txt" , "../f3.txt"];
+let files = ["./f1.txt" , "./f2.txt" , "./f3.txt"];
+
+for(let i =0;i<files.length;i++ ){
+    let filedata = fs.readFileSync(files[i],"utf8");
+    console.log(filedata);
+}
